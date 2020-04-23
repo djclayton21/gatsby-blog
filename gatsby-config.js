56231beab1ae081109beb6de55b4384e52ceae67
djclayton21 <clayton.daniel.j@gmail.com>
2020-04-23 17:28:27 -0600

    netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,9 +36,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
-    `gatsby-image`
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-image`,
+    `gatsby-plugin-netlify`
   ],
 };
